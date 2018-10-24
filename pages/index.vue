05e92900eb513471343bb4b0e07a2d5af79add39
keerthana suddala <ksuddala27@gmail.com>
2018-10-24 16:57:49 -0500

    added featuredimage,title and latest stories column to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,95 @@
 <template>
+  <!-- featured post -->
   <section class="section is-hero">
     <div class="container">
       <div class="columns is-vcentered">
         <div class="column is-6 is-5-fullhd is-offset-1-fullhd">
           <div class="section-header">
-            <h1 class="title is-spaced is-2"> The bedtime story<span class="is-hidden-touch"><br></span> app for families </h1>
-            <h2 class="subtitle is-5"> Awaken the inner storyteller with diverse and empowering stories for young &amp; old. Storytelling has never been easier. </h2>
+            <h1 class="title is-spaced is-2" > Government claims on<span class="is-hidden-touch"><br></span> the "Welfare of the Girl Child "</h1>
+            <h2 class="subtitle is-5">  An infographic published by the government claims that financial future has been secured for crores of girls in India. </h2>
             <a class="has-tint has-text-weight-semibold" href="/" target="_blank">
-              <i class="icon icon-award"></i><span>Parents' Choice Approved App</span>
+              <i class="icon icon-award"></i><span>BY AILA BANDAGI KANDLAKUNTA ON OCTOBER 10, 2018</span>
             </a>
-            <hr class="spacer is-1-5">
+            <hr class="spacer is-1-5" s>
           </div>
         </div>
         <div class="column is-6">
           <div class="section-media">
-            <img role="presentation" src="https://getbedtimestories.com/assets/images/hero/Hero-Karlotta@2x-cc57e26e.jpg?1540201404" srcset="https://getbedtimestories.com/assets/images/hero/Hero-Karlotta@2x-cc57e26e.jpg?1540201404 1x, https://getbedtimestories.com/assets/images/hero/Hero-Karlotta@2x-cc57e26e.jpg?1540201404 2x">
+            <img role="presentation" src="https://2nafqn3o0l6kwfofi3ydj9li-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/Welfare-of-the-Girl-Child_factly.png"width="500">
             <p> <small><a href="/library/karlotta-the-knight"><u>Karlotta the Knight</u></a> with her squire and steed.</small> </p>
-          </div>
+
+       </div>
+          <!-- latest stories column code -->
+
+     <div class="block" style="margin-top:50px;" >
+            
+            <div class="columns is-mobile" style="margin-left: -580px;text-align: justify-all;">
+              
+              <div class="column is-8-tablet is-4-desktop is-3-fullhd "  >
+                <div class="card"  >
+              <div class=" card-content">
+       
+          <h3>LATEST STORIES</h3> 
+          <br>
+         <h3> <strong>Podcast: Regimes weaponizing social media</strong> <br>
+9 mins ago / Business</h3> <br>
+<h3>
+  
+<strong>Democrats launch campaign to reach 1 million California voters before Election Day  </strong><br>
+32 mins ago / Politics </h3><br>
+
+<h3><strong>Facebook Messenger simplifies design for its 1.3 billion users </strong> <br>
+1 hour ago / Technology </h3><br>
+
+<h3>
+ <strong>Megyn Kelly: When I was a kid, Halloween blackface "was OK"  </strong><br>
+1 hour ago / U.S. News </h3><br>
+
+
+<h3> <strong>125 Android apps caught up in million dollar ad fraud scheme  </strong><br>
+1 hour ago / Technology  </h3><br> 
+<h3>
+<strong>Georgia governor candidate burned state’s Confederate-inspired flag </strong><br>
+2 hours ago / Politics  </h3><br>
+<h3><strong>Facebook debuts political ad archive data report </strong> <br>
+2 hours ago / Politics </h3><br>
+<h3><strong>Trump to meet Putin in Paris in November</strong> <br>
+2 hours ago / World </h3> <br>
+<h3>
+<strong>Tim Cook: Tech must embrace privacy to hold public trust </strong> <br>
+2 hours ago / Technology  </h3><br>
+<h3>
+<strong>Report: Russia supported industrial controls cyberattack in 2017 </strong> <br>
+2 hours ago / Technology </h3>
+</div>
+
+</div>
+<div style="margin-top:-1200px;"></div>
+
+      </div>
+
+<!-- image code -->
+
+<div class="column is-6-tablet is-8-desktop is-5-fullhd is-flex">
+    <div class="card">
+       <div class="card-image">
+        <figure class="image is-3*1">
+          <div class="section-media">
+           <img src= "https://2nafqn3o0l6kwfofi3ydj9li-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/formalization-of-Economy_factly_featuredimage-1-1078x516.jpg" width="400">
+         </div>
+         </figure>
+
+          
+        </div>
+      </div>
+   </div>
+</div>
+</div>
         </div>
       </div>
     </div>
+
+        
   </section>
 </template>
 
@@ -40,3 +110,7 @@ export default {
   }*/
 }
 </script>
+
+<style>
+
+</style>
