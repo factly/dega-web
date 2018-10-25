3716d219dd4d2669e2d7e3242cae900756f73c5c
Shashi Deshetti <deshetti@gmail.com>
2018-10-25 13:55:41 -0500

    Made formatting changes to the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -196,106 +196,99 @@
     <hr class="spacer is-1-5">
 
     <!-- MORE STORIES Section -->
-
-
     <section class="section">
-    <div class="column is-8">
       <h3>MORE STORIES</h3>
-       <br>
-  <!-- FIRST-POST -->
-       <div class="container columns">
-               <!-- post-image  column-->
-         <div class= "column is-5">
-          <div class="card">
-             <div class="card-image">
-               <figure class ="image is-5by3">
-                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-               </figure>
-             </div>
-         </div>
-       </div>
-              <!-- post-title-column -->
-         <div class="column is-3">
-          <div class="content">
-              <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
-                <br>
-              <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
-               <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-          </div>
-         </div>
-               <!-- post-blockquote-column -->
-         <div class="column is-3">
-           <div class="content">
-              <p>Following the judgment of the SC in September, the ECI has now issued directions to candidates & political parties on the format to be followed for publication of pending criminal cases in newspapers & TV channels. Here is a look at all the changes.
-              </p>
-           </div>
-         </div>
-    </div>
-       <br>
-  <!-- SECOND-POST-->
+      <br>
+      <!-- FIRST POST -->
       <div class="container columns">
-             <!-- post-image  column-->
-        <div class= "column is-5">
-           <div class="card">
-             <div class="card-image">
-               <figure class ="image is-5by3">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-               </figure>
-             </div>
-         </div>
-      </div>
-             <!-- post-title-column -->
-        <div class="column is-3">
-           <div class="content">
-              <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
-                <br>
-              <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
-             <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <!-- post-image  column-->
+        <div class= "column is-4">
+        <div class="card">
+           <div class="card-image">
+             <figure class ="image is-5by3">
+               <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+             </figure>
            </div>
         </div>
-
-             <!-- post-blockquote-column -->
-        <div class="column is-3">
-          <div class="content">
-             <p>Following the judgment of the SC in September, the ECI has now issued directions to candidates & political parties on the format to be followed for publication of pending criminal cases in newspapers & TV channels. Here is a look at all the changes.
-             </p>
-          </div>
-        </div>
       </div>
-       <br>
-
-       <!-- THIRD POST -->
-      <div class="container columns">
-             <!-- post-image  column-->
-        <div class= "column is-5">
-           <div class="card">
-             <div class="card-image">
-               <figure class ="image is-5by3">
-                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-               </figure>
-             </div>
-          </div>
-       </div>
-             <!-- post-title-column -->
-          <div class="column is-3">
+         <!-- post-title-column -->
+         <div class="column is-4">
             <div class="content">
-              <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
-                <br>
-              <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
+                <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
+                  <br>
+                <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
                 <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
             </div>
-          </div>
-
-             <!-- post-blockquote-column -->
-         <div class="column is-3">
+         </div>
+         <!-- post-blockquote-column -->
+         <div class="column is-4">
            <div class="content">
               <p>Following the judgment of the SC in September, the ECI has now issued directions to candidates & political parties on the format to be followed for publication of pending criminal cases in newspapers & TV channels. Here is a look at all the changes.
               </p>
            </div>
          </div>
       </div>
-
-    </div>
+      <br>
+      <!-- SECOND POST -->
+      <div class="container columns">
+        <!-- post-image  column-->
+        <div class= "column is-4">
+          <div class="card">
+            <div class="card-image">
+              <figure class ="image is-5by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <!-- post-title-column -->
+        <div class="column is-4">
+          <div class="content">
+            <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
+            <br>
+            <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <!-- post-blockquote-column -->
+        <div class="column is-4">
+          <div class="content">
+            <p>Following the judgment of the SC in September, the ECI has now issued directions to candidates & political parties on the format to be followed for publication of pending criminal cases in newspapers & TV channels. Here is a look at all the changes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <br>
+      <!-- THIRD POST -->
+      <div class="container columns">
+        <!-- post-image  column-->
+        <div class= "column is-4">
+          <div class="card">
+            <div class="card-image">
+              <figure class ="image is-5by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <!-- post-title-column -->
+        <div class="column is-4">
+          <div class="content">
+            <p class="title is-4"> Details of pending criminal cases against candidates contesting Lok Sabha & Assembly elections will now be on Newspapers & TV</p>
+            <br>
+            <p class="subtitle is-6">BY RAKESH DUBBUDU </p>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <!-- post-blockquote-column -->
+        <div class="column is-4">
+          <div class="content">
+            <p>Following the judgment of the SC in September, the ECI has now issued directions to candidates & political parties on the format to be followed for publication of pending criminal cases in newspapers & TV channels. Here is a look at all the changes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <br>
 
     </section>
 
