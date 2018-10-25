cd58d26121cad74a73f33e5aec84397e34881953
Shashi Deshetti <deshetti@gmail.com>
2018-10-25 07:37:35 -0500

    Made changes to the latest stories section to make it responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,206 @@
 <template>
-  <!-- featured post -->
-  <section class="section is-hero">
-    <div class="container">
-      <div class="columns is-vcentered">
-        <div class="column is-6 is-5-fullhd is-offset-1-fullhd">
-          <div class="section-header">
-            <h1 class="title is-spaced is-2" > Government claims on<span class="is-hidden-touch"><br></span> the "Welfare of the Girl Child "</h1>
-            <h2 class="subtitle is-5">  An infographic published by the government claims that financial future has been secured for crores of girls in India. </h2>
-            <a class="has-tint has-text-weight-semibold" href="/" target="_blank">
-              <i class="icon icon-award"></i><span>BY AILA BANDAGI KANDLAKUNTA ON OCTOBER 10, 2018</span>
-            </a>
-            <hr class="spacer is-1-5" s>
+  <div>
+    <!-- featured post section-->
+    <section class="section is-hero">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column is-6 is-5-fullhd is-offset-1-fullhd">
+            <div class="section-header">
+              <h1 class="title is-spaced is-2" > Government claims on<span class="is-hidden-touch"><br></span> the "Welfare of the Girl Child "</h1>
+              <h2 class="subtitle is-5">  An infographic published by the government claims that financial future has been secured for crores of girls in India. </h2>
+              <a class="has-tint has-text-weight-semibold" href="/" target="_blank">
+                <i class="icon icon-award"></i><span>BY AILA BANDAGI KANDLAKUNTA ON OCTOBER 10, 2018</span>
+              </a>
+            </div>
+          </div>
+          <div class="column is-6">
+            <div class="section-media">
+              <img role="presentation" src="https://2nafqn3o0l6kwfofi3ydj9li-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/Welfare-of-the-Girl-Child_factly.png"width="500">
+              <p> <small><a href="/library/karlotta-the-knight"><u>Karlotta the Knight</u></a> with her squire and steed.</small> </p>
+            </div>
           </div>
         </div>
-        <div class="column is-6">
-          <div class="section-media">
-            <img role="presentation" src="https://2nafqn3o0l6kwfofi3ydj9li-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/Welfare-of-the-Girl-Child_factly.png"width="500">
-            <p> <small><a href="/library/karlotta-the-knight"><u>Karlotta the Knight</u></a> with her squire and steed.</small> </p>
-
-       </div>
-          <!-- latest stories column code -->
-
-     <div class="block" style="margin-top:50px;" >
-            
-            <div class="columns is-mobile" style="margin-left: -580px;text-align: justify-all;">
-              
-              <div class="column is-8-tablet is-4-desktop is-3-fullhd "  >
-                <div class="card"  >
-              <div class=" card-content">
-       
-          <h3>LATEST STORIES</h3> 
+      </div>
+    </section>
+    <hr class="spacer is-1-5">
+    <!-- Latest Stories Section -->
+    <section class="section">
+      <div class="container columns">
+        <div class="column is-4 has-background-light">
+          <h3>LATEST FACTCHECKS</h3>
           <br>
-         <h3> <strong>Podcast: Regimes weaponizing social media</strong> <br>
-9 mins ago / Business</h3> <br>
-<h3>
-  
-<strong>Democrats launch campaign to reach 1 million California voters before Election Day  </strong><br>
-32 mins ago / Politics </h3><br>
-
-<h3><strong>Facebook Messenger simplifies design for its 1.3 billion users </strong> <br>
-1 hour ago / Technology </h3><br>
-
-<h3>
- <strong>Megyn Kelly: When I was a kid, Halloween blackface "was OK"  </strong><br>
-1 hour ago / U.S. News </h3><br>
-
-
-<h3> <strong>125 Android apps caught up in million dollar ad fraud scheme  </strong><br>
-1 hour ago / Technology  </h3><br> 
-<h3>
-<strong>Georgia governor candidate burned state’s Confederate-inspired flag </strong><br>
-2 hours ago / Politics  </h3><br>
-<h3><strong>Facebook debuts political ad archive data report </strong> <br>
-2 hours ago / Politics </h3><br>
-<h3><strong>Trump to meet Putin in Paris in November</strong> <br>
-2 hours ago / World </h3> <br>
-<h3>
-<strong>Tim Cook: Tech must embrace privacy to hold public trust </strong> <br>
-2 hours ago / Technology  </h3><br>
-<h3>
-<strong>Report: Russia supported industrial controls cyberattack in 2017 </strong> <br>
-2 hours ago / Technology </h3>
-</div>
-
-</div>
-<div style="margin-top:-1200px;"></div>
-
-      </div>
-
-<!-- image code -->
-
-<div class="column is-6-tablet is-8-desktop is-5-fullhd is-flex">
-    <div class="card">
-       <div class="card-image">
-        <figure class="image is-3*1">
-          <div class="section-media">
-           <img src= "https://2nafqn3o0l6kwfofi3ydj9li-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/formalization-of-Economy_factly_featuredimage-1-1078x516.jpg" width="400">
-         </div>
-         </figure>
-
-          
+          <div>
+            <h3><strong>Podcast: Regimes weaponizing social media</strong> <br>9 mins ago / Business</h3>
+          </div>
+          <br>
+          <div>
+            <h3><strong>Democrats launch campaign to reach 1 million California voters before Election Day  </strong><br>
+            32 mins ago / Politics </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>Facebook Messenger simplifies design for its 1.3 billion users </strong> <br>
+            1 hour ago / Technology </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>Megyn Kelly: When I was a kid, Halloween blackface "was OK"  </strong><br>
+            1 hour ago / U.S. News </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>125 Android apps caught up in million dollar ad fraud scheme  </strong><br>
+            1 hour ago / Technology  </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>Georgia governor candidate burned state’s Confederate-inspired flag </strong><br>
+            2 hours ago / Politics  </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>Facebook debuts political ad archive data report </strong> <br>
+            2 hours ago / Politics </h3>
+          </div>
+          <br>
+          <div>
+          <h3><strong>Trump to meet Putin in Paris in November</strong> <br>
+            2 hours ago / World </h3>
+          </div>
+          <br>
+          <div>
+          <h3>
+            <strong>Tim Cook: Tech must embrace privacy to hold public trust </strong> <br>
+            2 hours ago / Technology  </h3>
+          </div>
+          <br>
+          <div>
+          <h3>
+            <strong>Report: Russia supported industrial controls cyberattack in 2017 </strong> <br>
+            2 hours ago / Technology
+          </h3>
+          </div>
         </div>
-      </div>
-   </div>
-</div>
-</div>
+        <div class="column is-8">
+          <h3>LATEST STORIES</h3>
+          <br>
+          <div class="columns">
+            <div class="column is-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">John Smith</p>
+                      <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column is-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">John Smith</p>
+                      <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          <div class="columns">
+            <div class="column is-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">John Smith</p>
+                      <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column is-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">John Smith</p>
+                      <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+
         </div>
-      </div>
-    </div>
+    </section>
+  </div>
+
+
+
+
 
         
-  </section>
+
 </template>
 
 <script>
