701a32f0e0d824e8d9e2f0c2d2dc0c62da6101db
KriAga <kriaga3@gmail.com>
2019-01-30 03:56:46 +0530

    Fact check details page, assets/rating/images renaming

--- pages/factCheck/_slug/index.vue
+++ pages/factCheck/_slug/index.vue
@@ -1,12 +1,14 @@
 <template>
 <div>
-	<section class="section">
+	<section class="section"><br><br><br>
 		<div class="container columns has-text-centered">
 			<div class="column is-three-quarters">
-				<div class="box"><br><br><br>
-                    <h1 class="title is-size-5 is-size-4-tablet is-size-3-desktop is-spaced is-2 has-text-weight-bold">{{factcheck}}</h1>
+				<div class="box">
+					<h1 class="title is-size-5 is-size-4-tablet is-size-3-desktop is-spaced is-2 has-text-weight-bold">{{factchecks[0].title}}</h1>
 					<span class="is-uppercase">
-						BY
+						<!-- BY -->
+						<a class="has-tint has-text-weight-semibold " href="http://127.0.0.1:3000/author/shashi-deshetti" target="_blank" style="text-align: center">
+							<i class="icon icon-award"></i><span >BY SHASHI DESHETTI</span> </a>
 						<!-- <nuxt-link class="has-tint has-text-weight-semibold" :to="'/author/'+ factchecks[0].authors[0].slug">{{factchecks[0].authors[0].display_name}}</nuxt-link> -->
 						<div class="has-text-centered">{{getDate(factchecks[0].last_updated_date)}}</div>
 					</span>
@@ -16,119 +18,76 @@
 					<br>
 					<h2 class="subtitle is-size-6 is-size-6-tablet is-size-5-desktop has-text-weight-semibold">{{factchecks[0].excerpt}}</h2>
 				</div>
-			</div>
+			</div><br><br><br>
 			<div class="column is-one-fourth is-hidden-mobile">
 				<div class="box has-text-weight-semibold" style="color: #1976d2;text-align:left;">
 					Fact Check For The Claims
-					<br><br>
-					<h1 class="subtitle has-text-weight-bold" style="color:#034f84;">#1. Swiss Bank deposits have gone down</h1>
-					<p style="color:grey">The government claimed that Swiss Bank deposits by Indians decreased during the current government while they increased during the UPA regime.</p>
-					<br><br> <hr><br><br>
-					<h1 class="subtitle has-text-weight-bold" style="color:#034f84;">#2. More deposits in UPA regime</h1>
-					<p style="color:grey">The government claimed that Swiss Bank deposits by Indians decreased during the current government while they increased during the UPA regime.</p>
-					<br><br><br>
+					<div v-for="(claim, index) in factchecks[0].claims" :key="index">
+						<h1 class="has-text-weight-semibold" style="color:#034f84;" >#{{index+1}}. {{claim.claim}}</h1>
+						<!-- <p style="color:grey" v-html="claim.description">{{claim.description}}</p> -->
+						<hr>
+					</div>
 				</div>
 			</div>
 		</div>
-	<div class="container columns">
-				<div class="column is-three-quarters">
-					<div class="box">
-						<div class="columns is-mobile has-text-centered">
-			<!-- CLAIM COLUMN -->
-							<div  class="column is-three-quarters">
-								<p class="title is-size-5 is-size-4-tablet is-size-4-desktop is-spaced is-2" style="color: #1976d2">
-								<strong>CLAIM #1</strong></p>
-								<p class="title is-size-5 is-size-4-tablet is-size-4-desktop is-spaced is-2">Swiss bank deposits have gone down</p>
-								<p class="subtitle is-size-6 is-size-6-tablet is-size-5-desktop">The government claimed that Swiss Bank deposits by Indians decreased during the current government while they increased during the UPA regime.</p>
-								<p style="color: #1976d2;font-weight: bold;">Claimed by Narendra Modi On 31 Aug 2018 </p>
-								</div>
-								<!-- VALIDATION COLUMN -->
-								<div  class="column is-one-quarter">
-								<p class= "title is-size-5 is-size-4-tablet is-size-4-desktop is-spaced is-2 " style="color: #1976d2"><strong>VALIDATION</strong></p>
-								<h1 class="subtitle is-size-6 is-size-6-tablet is-size-1-desktop" style="color:LimeGreen">4.0</h1>
-								<span class="icon has-text-star has-text-centered" style="color:LimeGreen">
-								<svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-								<i class="fas fa-star"></i>
-								</span>
-
-								<span class="icon has-text-star has-text-centered" style="color:LimeGreen">
-								<svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-								<i class="fas fa-star"></i>
-								</span>
-
-								<span class="icon has-text-star has-text-centered" style="color:LimeGreen">
-								<svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-								<i class="fas fa-star"></i>
-								</span>
-
-								<span class="icon has-text-star has-text-centered" style="color:LimeGreen">
-								<svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-								<i class="fas fa-star"></i>
-								</span>
-
-								<span class="icon has-text-star has-text-centered">
-								<svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-								<i class="fas fa-star"></i>
-								</span>
-
-		         				<br> <br>
-								<span style="color:#1976d2">Mostly True</span>
-							</div>
+		<div class="container columns">
+			<div class="column is-three-quarters">
+				<!-- CLAIM COLUMN -->
+				<div class="box" v-for="(claim, index) in factchecks[0].claims" :key="index">
+					<div class="columns is-mobile has-text-left">
+						<div class="column is-one-fifth" style="float:left;">
+							<img :src="claim.claim_source" alt="Claim Source">
+						</div>
+						<div class="column is-three-fifth">
+							<p style="color: #1976d2;">Claimed by {{claim.claimant.name}}</p>
+							<p class="subtitle is-size-3 is-size-4-tablet is-size-5-desktop is-spaced is-2">{{claim.claim}}</p>
+							<!-- <p class="subtitle is-size-6 is-size-6-tablet is-size-5-desktop" v-html="claim.review">{{claim.review}}</p> -->
+						</div>
+						<div class="column is-one-fifth">
+							<img :src="claim.rating.numeric_value.png">
+							<!-- Rating: {{claim.rating.numeric_value}} -->
+							<!-- <img :src="~assets/images/ratings/claim.rating.numeric_value" alt="Rating"> -->
+							<!-- <img style="{ backgroundImage: 'url(' + require(@/assets/imgages/ratings/${claim.rating.numeric_value}) + ')' }"> -->
 						</div>
 					</div>
-		<!-- ARTCILE CONTENT -->
-					<div class="box">
-						<article class="post" style="text-align: justify;">
-							<p>An infographic published by the government claims that financial future has been secured for crores of girls in India. This claim is part of a series on ‘speed and scale of transformation’ in India. Here is a fact check of the claim.
-							</p>
-
-							<p><strong>What is the  Sukanya Samriddhi Scheme?</strong></p>
-
-							<p>The Sukanya Samriddhi Yojana (SSY) is as girl child prosperity scheme under the Beti Bachao Beti padhao program. The scheme is targeted at the girl child and her welfare. The natural or legal guardian of the girl child can open a bank account from the time of her birth till she attains an age of ten. The scheme requires the family to deposit small amounts into a bank account on the girl’s name. The current interest rate offered for this scheme by the government is 8.5% per annum. The normal tenure of the account is till the girl child turns 21 years of age where after she is entitled to the entire amount in the account. The scheme is administered by the Ministry of Finance and it was notified in December 2014.
-							</p>
-
-							<p><strong>Claim: More than 1.26 crore SSY accounts opened with Rs 19,183 crore deposits</strong></p>
-
-
-							<p>The minimum deposit amount under SSY has been reduced to Rs 250 from the existing Rs 1000. The scheme also provides for triple-E exemptions under the income tax act. The infograph claims that more than 1.26 crore  SSY accounts have been opened for the girl child with over Rs 19,183 crores deposited.</p>
-
-							<p>As on 30th June 2018, more than 1.39 crore have been opened under SSY as per the information shared by the government in Lok Sabha. The total deposits under SSY crossed Rs 25000 crore as of 30th June 2018.  There are wide state wise variations in terms of the accounts opened under SSY and the amount deposited. The highest average deposit per SSY account was in Delhi while the most number of accounts per 1000 female in the 0-10 age group was in Himachal Pradesh. Compared to the total female population in the 0-10 age group as per the 2011 census, the number of accounts opened is less than 15%.
-							</p>
-
-							<p>The interest rate on the accounts opened under this scheme when it was initially launched was 9.1% and was increased to 9.2% in the subsequent year. It was reduced to 8.1%  in 2017-18 and is now increased to 8.5% per annum.
-							</p>
-
-							<p><strong>Did the previous governments have similar schemes?</strong><br>
-							While the larger Beti Bachao Beti Padhao scheme has a number of schemes similar to the ones started before 2014, the SSY is a new approach. The UPA government had also implemented multiple schemes for the welfare of the girl child like the Dhanalakshmi (conditional cash transfer scheme), SABLA for nutrition of adolescent girls, special focus on girl child in Sarva Shiksha Abhian etc. But there was no small savings scheme like the SSY exclusively for the girl child.
-							</p>
-
-							<p>This story is part of a larger series on the 4-years of the Modi government. This series has been made possible with the flash grant of the International Fact Checking Network (IFCN). Read the rest of the stories in this series here.</p>
-						</article>
-					</div>
 				</div>
-		<!--RELATED ARTICLES BOX  -->
-				<div class="column is-one-fourth is-hidden-mobile">
-					<div class="box has-text-weight-semibold" style="color: #1976d2;">
-						Related Articles  <br><br>
-						<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">INDIA</h2>
-						<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
-
+				<!-- ARTCILE CONTENT -->
+				<div class="box">
+					<article class="post" style="text-align: justify;">
+						<p v-html="factchecks[0].introduction">{{factchecks[0].introduction}}</p>
+					</article>
+					<div v-for="(claim,index) in factchecks[0].claims" :key="index">
 						<br>
-
-						<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">ELECTIONS 2019</h2>
-						<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
+						<article class="post" style="text-align: justify;">
+							<p v-html="claim.description">{{claim.description}}</p>
+						</article>
 						<br>
-
-						<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">WORLD</h2>
-						<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
-						<br>
-						<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">BUSSINESS</h2>
-						<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
 					</div>
+					<article class="post" style="text-align: justify;">
+						<p>{{factchecks[0].summary}}</p>
+					</article>
 				</div>
 			</div>
-		</section>
-	</div>
+			<!--RELATED ARTICLES BOX  -->
+			<div class="column is-one-fourth is-hidden-mobile">
+				<div class="box has-text-weight-semibold" style="color: #1976d2;">
+					Related Articles  <br><br>
+					<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">INDIA</h2>
+					<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
+					<br>
+					<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">ELECTIONS 2019</h2>
+					<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
+					<br>
+					<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">WORLD</h2>
+					<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
+					<br>
+					<h2 class="subtitle is-6 has-text-weight-bold" style="color:lightgrey">BUSSINESS</h2>
+					<p class="subtitle has-text-weight-bold">Are More people travelling in Airplanes than in AC trains?</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 </template>
 <script>
 import axios from 'axios'
@@ -136,14 +95,24 @@ import axios from 'axios'
 export default {
   components: {
   },
-  methods: {},
-  async asyncData() {
+  methods: {
+	validate({ params }){
+		return params.slug
+	},
+	getDate(datetime) {
+      let date = new Date(datetime);
+      var ms = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      return date.getDate() + ' ' + ms[date.getMonth()] + ' ' + date.getFullYear();
+    }
+  },
+  async asyncData(params) {
+    console.log(params.params.slug)
     const config = {
-      url: 'http://127.0.0.1:8000/core/api/posts',
+      url: 'http://127.0.0.1:8080/core/api/posts',
       method: 'get',
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwUXo5YUVJVndiNmZrNUFDaVRhMTFaTU1JcC13QXRRak5ZYlU2OEtJVmlzIn0.eyJqdGkiOiI1ZjcxYmY2Ny02ZGVhLTQ2MmQtYTU4ZS0zMmViYzI0NTE4M2IiLCJleHAiOjE1NDI4MjM4NjcsIm5iZiI6MCwiaWF0IjoxNTQyODIzNTY3LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwODAvYXV0aC9yZWFsbXMvamhpcHN0ZXIiLCJhdWQiOiJmYWN0bHkiLCJzdWIiOiI3ZDg0Nzc4YS1jMGJhLTQ0NTEtYjBlZS04MDkyMzM5M2Y4YTAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJmYWN0bHkiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiIwNzFjYmIzOC01Y2Y5LTQwOTItYWNmYy0yNjIwM2Y0MDMwNjEiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjbGllbnRIb3N0IjoiMTcyLjI0LjAuMSIsImNsaWVudElkIjoiZmFjdGx5IiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LWZhY3RseSIsImNsaWVudEFkZHJlc3MiOiIxNzIuMjQuMC4xIiwiZW1haWwiOiJzZXJ2aWNlLWFjY291bnQtZmFjdGx5QHBsYWNlaG9sZGVyLm9yZyJ9.oj635dBWUs6aA28iOKW9XDJtZg-vT_3KueonJOxv4eKm_m_-mKwLxFenLG5F6xqpREPmMY89-Yl6Yo7683ALO9FSdjf6KSGdMaRVMLMxSTYRjKrpDHz7F0F0mn2fIl54AAdnl5CEiJ9Ol2dOdJBQFngRFQs8-Ww8Wkm0ZEmfIND4B_hLFgA5QeO0dzG6ZzCJiV3vY1yNZ1vvZPct5s_5v4D1bdNFLnrRURtzqITmQ9Ug-JMAQZ6nCdna6X-lJVkrNUD8sFm1dMGB2ixc73EYuhtsstk-j_5sa_3dsLnXgLLcTXcJ-EPTzyByznSN44xKceeOs-OX9Qydy3rKt6xzgQ'
+          'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwUXo5YUVJVndiNmZrNUFDaVRhMTFaTU1JcC13QXRRak5ZYlU2OEtJVmlzIn0.eyJqdGkiOiIzNWNmYTBjZS1hY2FmLTQyNjAtYmRjMC1iNWQ4ZjNjNjM2NDgiLCJleHAiOjE1NDI4NTk4NDksIm5iZiI6MCwiaWF0IjoxNTQyODIzODQ5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwODAvYXV0aC9yZWFsbXMvamhpcHN0ZXIiLCJhdWQiOiJmYWN0bHkiLCJzdWIiOiI3ZDg0Nzc4YS1jMGJhLTQ0NTEtYjBlZS04MDkyMzM5M2Y4YTAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJmYWN0bHkiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiJiZWQyY2RjMy1kYjE1LTQ1ZWQtODI4My03MDZiNDAyMDdjY2YiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjbGllbnRIb3N0IjoiMTcyLjI0LjAuMSIsImNsaWVudElkIjoiZmFjdGx5IiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LWZhY3RseSIsImNsaWVudEFkZHJlc3MiOiIxNzIuMjQuMC4xIiwiZW1haWwiOiJzZXJ2aWNlLWFjY291bnQtZmFjdGx5QHBsYWNlaG9sZGVyLm9yZyJ9.XRmqklBLNllMzcqWeAIJ13GFPGktlUkCDCy-DISKubvA1GFyd3j3Tbk1-9iswwcFORGuwDXZUD9cH1542qjzBNpgwhOVfWDzuMaOaJe2mLqEy_PM2mUAWvsr9N47wOvuETI6eN2BwxifWgG2ra2xqz4Gfr6lCxbKATLpcogJ3A0P-stSERxy23mJBH2QkHEyFdrX7HuFd6xpiwM0BU9iGuw9VmoW2Eu7Xvetq0Z56W8a4K9tzd5hvYkut-Ee5HcMpkxNH3KpsLCOye4Hxr6dU_I0h-hkFjuyCuFwzgf-w8TaIpOFp4YUEabW6PC5J21UYw7PiScbzzmoTBCyrtNFxQ'
       }
     }
     return axios
