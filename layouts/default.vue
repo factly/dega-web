79603ab3615545182b9e554b3c11880e5358058a
KriAga <kriaga3@gmail.com>
2019-02-17 01:32:58 +0530

    footer template

--- layouts/default.vue
+++ layouts/default.vue
@@ -85,6 +85,18 @@
       </div> -->
   <!-- </div> -->
   <!-- </div> -->
+
+      <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>DEGA</strong> by
+          <a href="https://factly.in/">FACTLY</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+      </div>
+    </footer>
+
+    
   </div>
 </template>
 
