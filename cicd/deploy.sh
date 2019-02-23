d07585fd66e27e703be877bc3489d5da258e1f1d
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-23 10:41:57 -0600

    testing docker push with travis

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Starting Logging into Docker"
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "Logged into Docker"
+echo "Logged into Docker"
 docker push $IMAGE
+echo "Image pushed to Docker Hub: {$IMAGE}"
