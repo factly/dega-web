d8612a530b204bfefedcf0495831b559e81e1e74
Krishna Agarwal <kriaga3@gmail.com>
2019-04-19 17:38:55 +0800

    List of claims now a component Sticky property fixed

--- pages/factcheck/_slug/index.vue
+++ pages/factcheck/_slug/index.vue
@@ -3,56 +3,26 @@
     <Hero :story="factchecks[0]" :categories= "true"/>
     <div class="column is-divider is-hidden-mobile is-offset-one-quarter is-half"/>
     <div class="columns">
-      <div
-        class="column is-three-fifths is-offset-1 is-full-mobile "
-        style="float:left;">
-
+      <div class="column is-1"/>
+      <div class="column is-full-mobile">
         <div class="column is-full">
-          <!-- ARTCILE CONTENT -->
           <div>
-            <article
-              class="post"
-              style="text-align: justify;">
+            <article class="post" style="text-align: justify;">
               <p v-html="factchecks[0].introduction">{{ factchecks[0].introduction }}</p>
             </article>
             <div v-for="(claim,index) in factchecks[0].claims" :key="index">
               <ClaimWidget :claim="claim" :index="index"/>
             </div>
-            <article
-              class="post"
-              style="text-align: justify;">
+            <article class="post" style="text-align: justify;">
               <p v-html="factchecks[0].summary">{{ factchecks[0].summary }}</p>
             </article>
           </div>
         </div>
       </div>
-      <div class="is-divider-vertical is-hidden-mobile"/>
-      <div class="column is-two-fifths is-hidden-mobile">
-        <div class="column is-half sticky">
-          <div
-            class="box has-text-weight-semibold"
-            style="color: #1976d2; text-align:left;">
-            <div v-if="factchecks[0].claims.length > 1">
-              <div class="is-uppercase has-text-centered">List of claims in the story</div>
-            </div>
-            <div v-else>
-              <div class="is-uppercase has-text-centered">Claims in the story</div>
-            </div>
-            <br>
-            <div class="has-text-centered">
-              <SocialSharing :url="$nuxt.$route.path"/>
-            </div>
-            <div
-              v-for="(claim, index) in factchecks[0].claims"
-              :key="index">
-              <hr>
-              <a :href="'#claim'+(index+1)"> 
-                <h2 class="has-text-weight-semibold" style="color:#034f84;" >#{{ index+1 }}. {{ claim.claim }}</h2>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div v-if="factchecks[0].claims.length > 1" class="column is-one-quarter is-hidden-mobile">
+        <ListClaims :factchecks="factchecks"/> 
       </div>
+      <div class="column is-1"/>
     </div>
     <SocialSharingVertical class="is-hidden-mobile" :url="$nuxt.$route.path"/>
   </section>
@@ -65,17 +35,20 @@ import Hero from '~/components/Hero';
 import ClaimWidget from '~/components/ClaimWidget';
 import SocialSharing from '~/components/SocialSharing';
 import SocialSharingVertical from '~/components/SocialSharingVertical';
+import ListClaims from '~/components/ListClaims.vue';
 
 export default {
   components: {
     Hero,
     ClaimWidget,
     SocialSharing,
-    SocialSharingVertical
+    SocialSharingVertical,
+    ListClaims
   },
   data() {
     return {
-      factchecks: null
+      factchecks: null,
+      ListClaimsHidden: false
     };
   },
   methods: {
@@ -102,6 +75,7 @@ export default {
     }
   },
   async asyncData(params) {
+    
     const factcheck = await axios
       .get(
         `${process.env.apiUri}/api/v1/factchecks/?client=${
