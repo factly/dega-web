364ce9aefada780a7cfe3d780251098be264cd1a
Krishna Agarwal <kriaga3@gmail.com>
2019-05-23 02:29:09 +0530

    ga plugin cleaned up

--- plugins/ga.js
+++ plugins/ga.js
@@ -19,14 +19,14 @@ async function getGATracking(app) {
 }
 
 export default ({ app }) => {
-    /*
-    ** Only run on client-side and only in production mode
-    */
-    // if (process.env.NODE_ENV !== 'production') return
-    /*
-    ** Include Google Analytics Script
-    */
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  /*
+  ** Only run on client-side and only in production mode
+  */
+  // if (process.env.NODE_ENV !== 'production') return
+  /*
+  ** Include Google Analytics Script
+  */
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
