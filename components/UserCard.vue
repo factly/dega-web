82138d94e9fb450e66c5d169c27dce03f0a025ac
Monark <monark@factly.in>
2019-09-25 15:40:01 +0530

    collection new ui added

---
+++ components/UserCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-96x96">
+            <img
+              :src="collection.media.sourceURL"
+              alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{ collection.displayName }}</p>
+        </div>
+      </div>
+
+      <div class="content">{{ collection.description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    collection: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
