e0396c535797a4a9b47d97e81ce147295f2fb1bf
shreeharsha-factly <harsha@factly.in>
2020-04-11 14:59:10 +0530

    social networks added to schema

--- graphql/query/organization.js
+++ graphql/query/organization.js
@@ -5,6 +5,15 @@ export const organizationQuery = `query {
         name
         site_title
         site_address
+        description
+        google_verification_code
+        facebook_url
+        twitter_url
+        instagram_url
+        linked_in_url
+        pinterest_url
+        youTube_url
+        github_url
         mediaLogo {
             source_url
             alt_text
